f9630b32a5bca72d0ce81b3a14cfb9cff149f090
internousdev <internousdev@103163801>
2017-11-06 15:16:06 +0900

    RandomFormのコメント挿入

--- src/com/internousdev/ECsite/util/RandomForm.java
+++ src/com/internousdev/ECsite/util/RandomForm.java
@@ -4,8 +4,19 @@ import java.security.SecureRandom;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+/**
+ * 無作為数列/無作為文字列生成クラス
+ * @author internousdev
+ *
+ */
 public class RandomForm {
 
+
+	/**
+	 * 1～9桁までのint形の無作為数列を生成。頭数桁0になる可能性あり。
+	 * @param i 任意の桁数
+	 * @return 無作為数列
+	 */
 	public int RandomNom(int i){
 		int result = 0;
 		if(i<10){
@@ -17,6 +28,11 @@ public class RandomForm {
 		return result;
 	}
 
+	/**
+	 * 任意の文字数の無作為文字列を生成。
+	 * @param i 任意の文字数
+	 * @return 無作為文字列
+	 */
 	public String RandomStr(int i){
 		String result = RandomStringUtils.randomAlphabetic(i);
 
